cd2b719e20bffc992d8e5aa64b29a9987c289dab
MunaFatinah <munafatinaha@gmail.com>
2026-01-13 17:13:46 +0700

    update project.java

--- project.java
+++ project.java
@@ -5,7 +5,7 @@ public class project {
         Long WaktuAwal = System.currentTimeMillis(); 
         while (t>0) {
             Long WaktuSekarang = System.currentTimeMillis(); 
-            if (WaktuSekarang-WaktuAwal >= 1000) {
+            if (WaktuSekarang-WaktuAwal >= 1000) { 
                 WaktuAwal = WaktuSekarang; 
                 int Menit = t / 60; 
                 int Detik = t % 60; 
@@ -13,11 +13,13 @@ public class project {
                 t--;
             }
         }
-        System.out.println("Waktu belajar selesai! Selamat istirahat");
+        System.out.println("Waktu belajar selesai! Selamat istirahat!");
     }
 
     public static void Tampilan() {
-        System.out.println("=====Selamat Datang di Study Timer=====");
+        System.out.println("=============================================================");
+        System.out.println("----Selamat Datang di Study Timer----");
+        System.out.println("Study Timer hadir untuk membantu kamu fokus dalam belajar");
         System.out.println("1. Atur timer belajar yang kamu inginkan dalam menit");
         System.out.println("2. Keluar");
     }
@@ -25,6 +27,34 @@ public class project {
     public static void main(String[] args) {
         Scanner v = new Scanner(System.in); 
         int [] Timer = new int [20]; 
-        
-    }
-}
+        int PenghitungTimer = 0; 
+
+        while (true) {
+            Tampilan();
+            System.out.println("Ayo tentukan pilihanmu! Belajar atau tunda?");
+            System.out.println("Silahkan pilih opsi 1 atau 2");
+            System.out.println("=============================================================");
+            int Pilihan = v.nextInt(); 
+
+            if (Pilihan == 1) {
+                if (PenghitungTimer < Timer.length) {
+                    System.out.println("Silahkan masukkan waktu belajar! (dalam menit)");
+                    int WaktuMenit = v.nextInt(); 
+                    int WaktuDetik = WaktuMenit*60; 
+                    Timer [PenghitungTimer] = WaktuDetik; 
+                    PenghitungTimer++;
+                    System.out.println("Timer untuk " + WaktuMenit + " menit telah diset!");
+                    penghitungWaktu(WaktuDetik);
+                } else {
+                    System.out.println("Timer sudah mencapai batas maksimum.");
+                }
+            } else if (Pilihan == 2) {
+                System.out.println("Terima kasih sudah menggunakan Study Timer!");
+                break;  
+            } else {
+                System.out.println("Pilihan tidak valid. Coba lagi.");
+            }
+        }
+        v.close();
+ }
+} 
